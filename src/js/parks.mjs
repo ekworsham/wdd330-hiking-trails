48d5d7a9f83f02092472548a269132a3e42554f6
ekworsham <145511698+ekworsham@users.noreply.github.com>
2025-04-13 18:58:22 -0400

    Base CSS

--- src/js/parks.mjs
+++ src/js/parks.mjs
@@ -1,17 +1,19 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
-
-// Wrap in an <a> tag to make it clickable
 function parkCardTemplate(park) {
     return `
         <li class="park-card">
-            <h1>${park.fullName}</h1>
-            <p>${park.description}</p>
-            <a href="/weather/?lat=${park.latitude}&lon=${park.longitude}">Weather Details</a>
-        </li>
-    `;
-}
+            <h1>PARK FULL NAME<br>${park.fullName}</h1>
+            <h3>NAME<br>${park.name}. State: ${park.states}</h3>
+            <p>Description<br>${park.description}</p>
+            <p>Designation<br>${park.designation}</p>
+            <p>Park URL<br>${park.url}</p>
+            <p>Directions<br>${park.directionsInfo}</p>
 
+            <a class="latLong" href="/weather/?lat=${park.latitude}&lon=${park.longitude}">Weather Details</a>
+        </li>
+    `;   
+}
 export default class Parks {
     constructor(dataSource, parkList) {
         this.dataSource = dataSource;
@@ -25,8 +27,7 @@ export default class Parks {
 
     // render after doing the first stretch
     renderList(list) {
-        renderListWithTemplate(parkCardTemplate, this.parkList, list);
+        const parksToDisplay = list.slice(0, 10);
+        renderListWithTemplate(parkCardTemplate, this.parkList, parksToDisplay);
     }
-
-    
 }
